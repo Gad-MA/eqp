3f92a2744c998504ae75f0b656b2f10c4da0e5c1
Mohamed Gad <147990860+Gad-MA@users.noreply.github.com>
2025-10-02 07:52:47 +0300

    Delete path.py

--- path.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-
-def Path(path:str):
-    if(path.startswith("f")):
-        return pathlib.Path("https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/hub/parrots.png")
-
-    else:
-        return pathlib.Path(path)
